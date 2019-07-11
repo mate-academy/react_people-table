23d5ae1e19eadea169f5b51d781fa4b49d8a93ce
Nastia Krat <nastyakpi1995@gmail.com>
2019-07-11 07:49:40 +0300

    Solution

--- src/App.js
+++ src/App.js
@@ -1,9 +1,33 @@
 import React from 'react';
 
-const App = () => (
-  <div className="App">
-    <h1>People table</h1>
-  </div>
-);
+const getPeople = async() => {
+  const Api = 'https://mate-academy.github.io/react_people-table/api/';
+  return fetch(`${Api}/people.json`).then(respose => respose.json());
+};
+
+class App extends React.Component {
+  state = {
+    people: [],
+  }
+
+  async componentDidMount() {
+    const people = await getPeople();
+
+    this.setState({
+      people,
+    });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>
+        People table
+          { this.state.people.length }
+        </h1>
+      </div>
+    );
+  }
+}
 
 export default App;
