6135e0300e718546a315980a0b3087d1c43e78bb
Serhii <davydko.serhiy@gmail.com>
2019-07-11 15:58:11 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,36 @@
 import React from 'react';
 
-const App = () => (
-  <div className="App">
-    <h1>People table</h1>
-  </div>
-);
+const peopleFromServer = 'https://mate-academy.github.io/react_people-table/api/people.json';
+
+const getPeople = async () => {
+  const response = await fetch(peopleFromServer);
+  const people = await response.json();
+  return people;
+};
+
+class App extends React.Component {
+  state = {
+    people: [],
+    peopleCopy: [],
+    isLoaded: false,
+  }
+
+  async componentDidMount() {
+    const people = await getPeople();
+
+    this.setState({
+      people: people,
+    });
+  }
+
+  render() {
+
+    return (
+      <div>
+        <h1>{this.state.people.length}</h1>
+      </div>
+    )
+  }
+}
 
 export default App;
