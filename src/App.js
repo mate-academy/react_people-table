32ec1c18a732f6c4d986a9a615e360dbb3c88d7e
felsi13 <13felsi@gmail.com>
2019-07-15 23:13:00 +0300

    8,9

--- src/App.js
+++ src/App.js
@@ -3,9 +3,19 @@ import PeopleTable from './components/PeopleTable';
 import getPeople from './api/getPeople';
 import './App.css';
 
+const getSortedPeople = (people, sortField) => {
+  const callback = (typeof people[0][sortField] === 'string')
+    ? (personA, personB) => personA[sortField].localeCompare(personB[sortField])
+    : (personA, personB) => personA[sortField] - personB[sortField];
+
+  return [...people].sort(callback);
+};
+
 class App extends React.Component {
   state = {
     people: [],
+    visiblePeople: [],
+    sortField: 'id',
   };
 
   async componentDidMount() {
@@ -14,11 +24,21 @@ class App extends React.Component {
 
   loadData = async() => {
     const people = await getPeople();
-    this.setState({ people });
+    this.setState({
+      people,
+      visiblePeople: getSortedPeople(people, 'id'),
+    });
+  };
+
+  sortBy = (sortField) => {
+    this.setState({
+      visiblePeople: getSortedPeople(this.state.people, sortField),
+      sortField,
+    });
   };
 
   render() {
-    const { people } = this.state;
+    const { people, visiblePeople } = this.state;
     return (
       <div className="App">
         <h1>
@@ -27,8 +47,29 @@ class App extends React.Component {
           {`${people.length}`}
           )
         </h1>
+
+        <button type="button" onClick={() => this.sortBy('id')}>
+          Sort by ID
+        </button>
+
+        <button type="button" onClick={() => this.sortBy('name')}>
+          Sort by name
+        </button>
+
+        <button type="button" onClick={() => this.sortBy('age')}>
+          Sort by age
+        </button>
+
+        <button type="button" onClick={() => this.sortBy('born')}>
+          Sort by year of birth
+        </button>
+
+        <button type="button" onClick={() => this.sortBy('died')}>
+          Sort by year of death
+        </button>
+
         <PeopleTable
-          people={people}
+          people={visiblePeople}
         />
       </div>
     );
