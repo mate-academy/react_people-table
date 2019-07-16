046eccecdc6a2245345517168ccb9b03700fde0c
Sergii Kasian <kasyansv1@gmail.com>
2019-07-16 15:22:29 +0300

    1

--- src/App.js
+++ src/App.js
@@ -1,9 +1,38 @@
 import React from 'react';
 
-const App = () => (
-  <div className="App">
-    <h1>People table</h1>
-  </div>
-);
+const getPeople = async() => {
+  const url
+    = 'https://mate-academy.github.io/react_people-table/api/people.json';
+  const response = await fetch(url);
+  const people = await response.json();
+
+  return people;
+};
+
+class App extends React.Component {
+  state = {
+    people: [],
+  };
+
+  async componentDidMount() {
+    const people = await getPeople();
+
+    this.setState({ people });
+  }
+
+  render() {
+    const { people } = this.state;
+
+    return (
+      <div className='App'>
+        <h1>
+          Number of people:
+          &nbsp;
+          {people.length}
+        </h1>
+      </div>
+    );
+  }
+}
 
 export default App;
