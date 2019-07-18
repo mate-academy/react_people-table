ed73bbcc59194985bfb2b76f2bda51dbc956c816
Dmitry Yevlakhov <tragenstolz@gmail.com>
2019-07-18 16:31:16 +0300

    new version

--- src/components/PeopleTable/PeopleTable.js
+++ src/components/PeopleTable/PeopleTable.js
@@ -1,121 +1,68 @@
 /* eslint-disable no-shadow */
-import React, { Component } from 'react';
+import React from 'react';
 import { connect } from 'react-redux';
-import { peopleTablePropTypes } from '../propTypes';
-
-import './PeopleTable.css';
+import PropTypes from 'prop-types';
+import { prepareUsers, setFilter, setSort } from '../redux/users';
+import titles from '../api/titles';
 import Person from '../Person/Person';
-import { filterUsers, sortUsers } from '../redux/usersToShow';
-import { startAdding } from '../redux/newPerson';
 
-class PeopleTable extends Component {
-  state = {
-    sortDirection: false,
-    selectedPersonId: -1,
-  };
-
-  handleSelectPerson = id => this.setState({ selectedPersonId: id });
-
-  handleSort = (name) => {
-    const { sortDirection } = this.state;
-    const { sortUsers } = this.props;
-
-    sortUsers(name, sortDirection);
-    this.setState(prevState => ({
-      sortDirection: !prevState.sortDirection,
-    }));
-  };
-
-  render() {
-    const {
-      users,
-      usersToShow,
-      filterUsers,
-      isAddingNew,
-      startAdding,
-    } = this.props;
-
-    const titles = [
-      { id: 1, name: 'Id' },
-      { id: 2, name: 'Name' },
-      { id: 3, name: 'Sex' },
-      { id: 4, name: 'Age' },
-      { id: 5, name: 'Century' },
-      { id: 6, name: 'Born' },
-      { id: 7, name: 'Died' },
-      { id: 8, name: 'Mother' },
-      { id: 9, name: 'Father' },
-      { id: 10, name: 'Children' },
-    ];
-
-    return (
-      <div className="">
-        <h1 className="app-header">
-          {usersToShow.length
-            ? `${usersToShow.length} people found`
-            : 'People not found'}
-        </h1>
-        {isAddingNew || (
-          <button
-            type="button"
-            className="action-button"
-            onClick={startAdding}
-          >
-            Add a new person
-          </button>
-        )}
+const PeopleTable = ({ users, setFilter, setSort }) => (
+  <>
+    <h1 className="container-header">
+      {users.length ? `${users.length} people found` : 'People not found'}
+    </h1>
+    <form>
+      <label htmlFor="search-field">
         <input
+          id="search-field"
           type="text"
-          className="table-search"
+          className="search-field"
           placeholder="Search by name, mother`s or father`s name"
-          onChange={(event) => {
-            filterUsers(event.target.value, users);
-          }}
-          readOnly={isAddingNew}
+          onChange={e => setFilter(e.target.value)}
         />
-        <table className="table">
-          <thead>
-            <tr className="table__header">
-              {titles.map(header => (
-                <th
-                  name={header.name}
-                  key={header.id}
-                  onClick={() => this.handleSort(header.name.toLowerCase())}
-                >
-                  {header.name}
-                </th>
-              ))}
-            </tr>
-          </thead>
-          <tbody>
-            {usersToShow.map(user => (
-              <Person
-                key={user.id}
-                person={user}
-                selectedId={this.state.selectedPersonId}
-                handlePersonClick={this.handleSelectPerson}
-                titles={titles}
-              />
-            ))}
-          </tbody>
-        </table>
-      </div>
-    );
-  }
-}
+      </label>
+    </form>
+    <table className="table">
+      <thead>
+        <tr className="table__header">
+          {titles.map(title => (
+            <th
+              onClick={() => setSort(title.name.toLocaleLowerCase())}
+              key={title.id}
+              name={title.name}
+            >
+              {title.name}
+            </th>
+          ))}
+        </tr>
+      </thead>
+      <tbody>
+        {users.map(user => (
+          <Person key={user.id} person={user} />
+        ))}
+      </tbody>
+    </table>
+  </>
+);
 
-PeopleTable.propTypes = peopleTablePropTypes;
+PeopleTable.propTypes = {
+  users: PropTypes.arrayOf(PropTypes.shape()).isRequired,
+  setSort: PropTypes.func.isRequired,
+  setFilter: PropTypes.func.isRequired,
+};
 
-const mapState = ({ usersToShow, isAddingNew, users }) => ({
-  users,
-  usersToShow,
-  isAddingNew,
+const mapState = ({ users }) => ({
+  users: prepareUsers(
+    users.users,
+    users.filterField,
+    users.sortField,
+    users.sortDirection
+  ),
 });
 
 const mapDispatch = {
-  filterUsers,
-  sortUsers,
-  startAdding,
+  setFilter,
+  setSort,
 };
 
 export default connect(
--- src/components/redux/users.js
+++ src/components/redux/users.js
@@ -1,5 +1,8 @@
 const ACTION_TYPE_SET_USERS = 'SET_USERS';
 const ACTION_TYPE_ADD_USER = 'ADD_USER';
+const ACTION_TYPE_SET_SELECTED = 'SELECT_USER';
+const ACTION_TYPE_SET_FILTER = 'SET_FILTER';
+const ACTION_TYPE_SET_SORT = 'SET_SORT';
 
 export const setUsers = users => ({
   type: ACTION_TYPE_SET_USERS,
@@ -11,14 +14,59 @@ export const addUser = person => ({
   person,
 });
 
-const usersReducer = (state = [], action) => {
+export const setSelected = id => ({
+  type: ACTION_TYPE_SET_SELECTED,
+  id,
+});
+
+export const setFilter = field => ({
+  type: ACTION_TYPE_SET_FILTER,
+  field,
+});
+
+export const setSort = field => ({
+  type: ACTION_TYPE_SET_SORT,
+  field,
+});
+
+const initialState = {
+  users: [],
+  selectedPerson: 0,
+  filterField: '',
+  sortField: '',
+  sortDirection: true,
+};
+
+const usersReducer = (state = initialState, action) => {
   const { type } = action;
   switch (type) {
     case ACTION_TYPE_SET_USERS:
-      return action.users;
+      return {
+        ...state,
+        users: action.users,
+      };
 
     case ACTION_TYPE_ADD_USER:
-      return [...state].concat(action.person);
+      return state;
+
+    case ACTION_TYPE_SET_SELECTED:
+      return {
+        ...state,
+        selectedPerson: action.id,
+      };
+
+    case ACTION_TYPE_SET_FILTER:
+      return {
+        ...state,
+        filterField: action.field,
+      };
+
+    case ACTION_TYPE_SET_SORT:
+      return {
+        ...state,
+        sortDirection: !state.sortDirection,
+        sortField: action.field,
+      };
 
     default:
       return state;
@@ -26,3 +74,46 @@ const usersReducer = (state = [], action) => {
 };
 
 export default usersReducer;
+
+export const prepareUsers = (
+  users,
+  filterField,
+  sortField,
+  sortDirection
+) => {
+  let result = [...users];
+  if (sortField) {
+    const createSorterBy = (field, name) => (a, b) => {
+      switch (typeof a[field]) {
+        case 'string':
+          return a[field].localeCompare(b[field]);
+
+        case 'boolean':
+        case 'number':
+          return a[field] - b[field];
+
+        case 'object':
+          return name === 'Array' ? a[field].length - b[field].length : 0;
+
+        default:
+          return 0;
+      }
+    };
+    const { name } = users[0][sortField].constructor;
+    const callback = createSorterBy(sortField, name);
+    result = sortDirection
+      ? result.sort(callback).reverse()
+      : result.sort(callback);
+  }
+
+  if (filterField) {
+    result = result.filter(user => (
+      [user.name, user.mother, user.father]
+        .join('')
+        .toLowerCase()
+        .includes(filterField.trim().toLowerCase())
+    ));
+  }
+
+  return result;
+};
