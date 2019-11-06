fdec4ec82d0f832ed638b3044edccffeb2e94aa9
Karpyshyn Roman <karpyshyn.roman@gmail.com>
2019-11-06 18:08:14 +0200

    s

--- src/utils.js
+++ src/utils.js
@@ -1,4 +1,4 @@
 export const getData = async () => {
-  const response = await fetch('/api/people.json');
+  const response = await fetch('https://mate-academy.github.io/react_people-table/api/people.json');
   return response.json();
 };
