3dc87413ef11d03cd0454c9291a4b2e52565811b
Oles Marola <oles_pw@ukr.net>
2019-12-11 16:59:33 +0200

    add search

--- src/App.js
+++ src/App.js
@@ -3,15 +3,35 @@ import People from './people';
 import './App.scss';
 import PeopleTable from './PeopleTable';
 
+const peopleWithId = peopleList => peopleList.map(
+  (person, i) => ({
+    ...person,
+    id: i,
+    age: person.died - person.born,
+    century: Math.ceil(person.died / 100),
+  })
+);
+
 class App extends React.Component {
-  peopleWithId = peopleList => peopleList.map(
-    (person, i) => ({
-      ...person,
-      id: i,
-      age: person.died - person.born,
-      century: Math.ceil(person.died / 100),
-    })
-  );
+  people = peopleWithId(People);
+
+  state = {
+    people: this.people,
+    peopleCount: this.people.length,
+  };
+
+  handleInputChange = (e) => {
+    const searchQuery = e.target.value.trim().toLowerCase();
+    const result = this.people.filter(
+      person => (person.name + person.mother + person.father).toLowerCase()
+        .includes(searchQuery)
+    );
+
+    this.setState({
+      people: result,
+      peopleCount: result.length,
+    });
+  };
 
   render() {
     return (
@@ -19,9 +39,13 @@ class App extends React.Component {
         <h1>
           People table
           {' '}
-          { People.length }
+          {this.state.peopleCount}
         </h1>
-        <PeopleTable people={this.peopleWithId(People)} />
+        <input
+          type="search"
+          onChange={this.handleInputChange}
+        />
+        <PeopleTable people={this.state.people} />
       </div>
     );
   }
