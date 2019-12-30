36a2e6c29cee63d37669b50d6b008048394ebe57
YuliiaChupryna <julia.chuprina2907@gmail.com>
2019-12-30 04:56:22 +0200

    solution

--- src/PeopleTable.js
+++ src/PeopleTable.js
@@ -1,38 +1,103 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import cn from 'classnames';
 import Person from './Person';
+import getData from './getDataApi';
 
-class PeopleTable extends React.Component {
-  state = {
-    selectedPersonId: 0,
-  };
+const peopleURL
+  = 'https://mate-academy.github.io/react_people-table/api/people.json';
 
-  selectPerson = (id) => {
-    if (id === this.state.selectedPersonId) {
-      this.setState({ selectedPersonId: 0 });
-    } else {
-      this.setState({ selectedPersonId: id });
+const getPeopleWithChildren = listOfPeople => (
+  listOfPeople.map((person, index) => ({
+    ...person,
+    children: listOfPeople
+      .filter(child => (
+        child.father === person.name || child.mother === person.name)),
+    age: person.died - person.born,
+    century: Math.ceil(person.died / 100),
+    id: index + 1,
+  }))
+);
+
+const PeopleTable = ({ match, history, location }) => {
+  const [people, setPeople] = useState([]);
+
+  useEffect(() => {
+    loadPeopleFromServer();
+  }, []);
+
+  const loadPeopleFromServer = async() => {
+    try {
+      const peopleList = await getData(peopleURL);
+
+      setPeople([...getPeopleWithChildren(peopleList)]);
+    } catch {
+      setPeople([]);
     }
   };
 
-  render() {
-    const { people, setSortBy, sortingTitle } = this.props;
-    const { selectedPersonId } = this.state;
-    const titles = [
-      'id',
-      'name',
-      'sex',
-      'born',
-      'died',
-      'age',
-      'century',
-      'mother',
-      'father',
-      'children',
-    ];
+  const urlParams = new URLSearchParams(location.search);
 
-    return (
+  const handleSearchingInputChange = ({ target: { value } }) => {
+    urlParams.set('query', value.toLowerCase());
+    history.push({ search: urlParams.toString() });
+  };
+
+  const getSearchedPeople = (listOfPeople, searchingName) => (
+    listOfPeople.filter(person => (
+      person.name.toLowerCase().includes(searchingName)
+      || (person.father || '').toLowerCase()
+        .includes(searchingName)
+      || (person.mother || '').toLowerCase()
+        .includes(searchingName)
+    )));
+
+  const getSortedPeople = (listOfPeople, sortingTitle) => (
+    listOfPeople.sort((firstPerson, secondPerson) => (
+      typeof firstPerson[sortingTitle] === 'string'
+        ? firstPerson[sortingTitle].localeCompare(secondPerson[sortingTitle])
+        : firstPerson[sortingTitle] - secondPerson[sortingTitle]
+    )));
+
+  const setSortBy = (sortingTitle) => {
+    if (urlParams.get('sortBy') === sortingTitle
+      && urlParams.get('sortOrder') === 'asc') {
+      urlParams.set('sortOrder', 'desc');
+    } else {
+      urlParams.set('sortOrder', 'asc');
+    }
+
+    if (sortingTitle === urlParams.get('sortBy')) {
+      setPeople([...people].reverse());
+    } else {
+      setPeople([...getSortedPeople(people, sortingTitle)]);
+    }
+
+    urlParams.set('sortBy', sortingTitle);
+    history.push({ search: urlParams.toString() });
+  };
+
+  const titles = [
+    'id',
+    'name',
+    'sex',
+    'born',
+    'died',
+    'age',
+    'century',
+    'mother',
+    'father',
+    'children',
+  ];
+
+  return (
+    <>
+      <input
+        onChange={handleSearchingInputChange}
+        className="people__search"
+        type="text"
+        placeholder="Search"
+      />
       <table className="people-table">
         <thead>
           <tr>
@@ -50,7 +115,7 @@ class PeopleTable extends React.Component {
                       className={
                         cn(
                           'title--sortable',
-                          { 'title--active': sortingTitle === title }
+                          { 'title--active': urlParams.get('sortBy') === title }
                         )}
                       onClick={() => setSortBy(title)}
                     >
@@ -62,38 +127,35 @@ class PeopleTable extends React.Component {
           </tr>
         </thead>
         <tbody>
-          {people.map(currentPerson => (
-            <Person
-              key={currentPerson.id}
-              titles={titles}
-              person={currentPerson}
-              isSelected={currentPerson.id === selectedPersonId}
-              selectPerson={() => (this.selectPerson(currentPerson.id))}
-            />
-          ))}
+          {[...getSearchedPeople([...people], urlParams.get('query') || '')]
+            .map(currentPerson => (
+              <Person
+                key={currentPerson.id}
+                titles={titles}
+                person={currentPerson}
+                match={match}
+                history={history}
+                location={location}
+              />
+            ))}
         </tbody>
       </table>
-    );
-  }
-}
+    </>
+  );
+};
 
 PeopleTable.propTypes = {
-  people: PropTypes.arrayOf(PropTypes.shape({
-    id: PropTypes.number.isRequired,
-    name: PropTypes.string.isRequired,
-    sex: PropTypes.string.isRequired,
-    born: PropTypes.number.isRequired,
-    died: PropTypes.number.isRequired,
-    age: PropTypes.number.isRequired,
-    mother: PropTypes.string.isRequired,
-    father: PropTypes.string.isRequired,
-    children: PropTypes.arrayOf(PropTypes.shape({
-      name: PropTypes.string,
-      sex: PropTypes.string,
-    })).isRequired,
-  })).isRequired,
-  setSortBy: PropTypes.func.isRequired,
-  sortingTitle: PropTypes.string.isRequired,
+  match: PropTypes.shape({
+    params: PropTypes.shape({
+      personName: PropTypes.string,
+    }),
+  }).isRequired,
+  history: PropTypes.shape(
+    { push: PropTypes.func },
+  ).isRequired,
+  location: PropTypes.shape({
+    search: PropTypes.shape({}),
+  }).isRequired,
 };
 
 export default PeopleTable;
--- src/Person.js
+++ src/Person.js
@@ -3,85 +3,94 @@ import React from 'react';
 import cn from 'classnames';
 import PersonName from './PersonName';
 
-const Person = ({ titles, person, isSelected, selectPerson }) => (
-  <tr
-    className={
-      cn(
-        'person',
-        `person--lived-in-${person.century}`,
-        { 'person--selected': isSelected }
-      )}
-    onClick={selectPerson}
-  >
-    {titles.map((title) => {
-      switch (title) {
-        case 'name':
-          return (
-            <td className={cn(
-              { 'person--born-before-1650': person.born < 1650 },
-              { 'person--died-after-1800': person.died > 1800 },
-            )}
-            >
-              <PersonName name={person.name} sex={person.sex} />
-            </td>
-          );
-        case 'sex':
-          return (
-            <td className={cn(
-              { 'person--male': person.sex === 'm' },
-              { 'person--female': person.sex === 'f' },
-            )}
-            >
-              {person.sex}
-            </td>
-          );
-        case 'died':
-          return (
-            <td>{person.died < Infinity ? person.died : 'alive'}</td>
-          );
-        case 'age':
-          return (
-            <td
-              className={cn({ 'person--old': person.age >= 65 })}
-            >
-              {person.age}
-            </td>
-          );
-        case 'mother':
-          return (
-            <td>
-              {person.mother !== '' && (
-                <PersonName name={person.mother} sex="f" />
+const Person = ({ titles, person, match, history, location }) => {
+  const selectPerson = () => {
+    history.push({
+      pathname: `/people/${person.name.toLowerCase().split(' ').join('-')}`,
+      search: location.search,
+    });
+  };
+
+  return (
+    <tr
+      className={
+        cn(
+          'person',
+          `person--lived-in-${person.century}`,
+          {
+            'person--selected':
+              person.name.toLowerCase().split(' ').join('-')
+              === match.params.personName,
+          }
+        )}
+      onClick={selectPerson}
+    >
+      {titles.map((title) => {
+        switch (title) {
+          case 'name':
+            return (
+              <td className={cn(
+                { 'person--born-before-1650': person.born < 1650 },
+                { 'person--died-after-1800': person.died > 1800 },
               )}
-            </td>
-          );
-        case 'father':
-          return (
-            <td>
-              {person.father !== '' && (
-                <PersonName name={person.father} sex="m" />
+              >
+                <PersonName name={person.name} sex={person.sex} />
+              </td>
+            );
+          case 'sex':
+            return (
+              <td className={cn(
+                { 'person--male': person.sex === 'm' },
+                { 'person--female': person.sex === 'f' },
               )}
-            </td>
-          );
-        case 'children':
-          return (
-            <td>
-              {person.children.map((child, index) => (
-                <>
-                  <PersonName name={child.name} sex={child.sex} />
-                  {index !== person.children.length - 1 && ', '}
-                </>
-              ))}
-            </td>
-          );
-        default:
-          return (
-            <td>{person[title]}</td>
-          );
-      }
-    })}
-  </tr>
-);
+              >
+                {person.sex}
+              </td>
+            );
+          case 'age':
+            return (
+              <td
+                className={cn({ 'person--old': person.age >= 65 })}
+              >
+                {person.age}
+              </td>
+            );
+          case 'mother':
+            return (
+              <td>
+                {person.mother !== '' && (
+                  <PersonName name={person.mother} sex="f" />
+                )}
+              </td>
+            );
+          case 'father':
+            return (
+              <td>
+                {person.father !== '' && (
+                  <PersonName name={person.father} sex="m" />
+                )}
+              </td>
+            );
+          case 'children':
+            return (
+              <td>
+                {person.children.map((child, index) => (
+                  <>
+                    <PersonName name={child.name} sex={child.sex} />
+                    {index !== person.children.length - 1 && ', '}
+                  </>
+                ))}
+              </td>
+            );
+          default:
+            return (
+              <td>{person[title]}</td>
+            );
+        }
+      })}
+    </tr>
+  );
+};
 
 Person.propTypes = {
   titles: PropTypes.arrayOf(PropTypes.string.isRequired).isRequired,
@@ -99,8 +108,21 @@ Person.propTypes = {
       sex: PropTypes.string,
     })).isRequired,
   }).isRequired,
-  isSelected: PropTypes.bool.isRequired,
-  selectPerson: PropTypes.func.isRequired,
+  match: PropTypes.shape({
+    params: PropTypes.shape({
+      personName: PropTypes.string,
+    }),
+  }).isRequired,
+  history: PropTypes.shape(
+    { push: PropTypes.func },
+  ).isRequired,
+  location: PropTypes.shape({
+    search: PropTypes.shape({
+      sortBy: PropTypes.string,
+      sortOrder: PropTypes.string,
+      query: PropTypes.string,
+    }),
+  }).isRequired,
 };
 
 export default Person;
