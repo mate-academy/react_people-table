d70450730eae4912e9caa84b8ae808504ba1fa0d
ksukhaldun <kseniya.khaldun@gmail.com>
2020-01-06 16:24:09 +0300

    solution

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
 import React from 'react';
+import './App.css';
+import { Switch, Route, NavLink, HashRouter } from 'react-router-dom';
+import PeoplePage from './PeoplePage';
 
 const App = () => (
   <div className="App">
     <h1>People table</h1>
+    <HashRouter>
+      <p className="ui secondary  menu">
+        <NavLink className="item" to="/" exact>Home</NavLink>
+        <NavLink to="/people" exact className="item">People</NavLink>
+      </p>
+      <Switch>
+        <Route exact path="/" render={() => (<></>)} />
+        <Route path="/people/:name?" component={PeoplePage} />
+      </Switch>
+    </HashRouter>
   </div>
 );
 
