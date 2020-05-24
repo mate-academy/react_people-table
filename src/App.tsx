c98fa43b5e0c222c01a5505bba1ae79963030d54
Vlad <oneviddi@gmail.com>
2020-05-24 11:31:29 +0300

    initial 1-4 done

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,38 @@
 import React from 'react';
+import {
+  HashRouter,
+  Switch,
+  Route,
+  NavLink,
+  Redirect,
+} from 'react-router-dom';
 
 import './App.css';
 
+const Home = () => {
+  return (
+    <h2>Home Page perhaps</h2>
+  );
+};
+
+const People = () => <h2>People Page perhaps</h2>;
+
 const App = () => (
-  <div className="App">
-    <h1>People table</h1>
-  </div>
+  <HashRouter>
+    <div className="App">
+      <h1>People table</h1>
+      <NavLink to="/">Transfer to Home</NavLink>
+      <NavLink to="/people">Transfer to People</NavLink>
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/home"><Redirect to="/" /></Route>
+        <Route path="/people" component={People} />
+        <>
+          <h2>Requested page has not been created yet</h2>
+        </>
+      </Switch>
+    </div>
+  </HashRouter>
 );
 
 export default App;
