eab7ed68e37d075c837f535640c053c3688aa4d0
kate <kate@test.com>
2020-05-28 18:09:58 +0300

    debounce added

--- src/PeoplePage.tsx
+++ src/PeoplePage.tsx
@@ -1,7 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import { useHistory, useLocation } from 'react-router-dom';
+import debounce from 'lodash.debounce';
+
 import { PeopleTable } from './PeopleTable';
 import { getPeople } from './api';
+
 
 function preparePerson(person: Person, all: Person[]): Person {
   return {
@@ -25,9 +28,17 @@ export const PeoplePage = () => {
 
   const history = useHistory();
   const location = useLocation();
+  const applyFilter = (value: string) => {
+    history.push({
+      search: `?query=${value}`,
+    });
+  };
+
+  const historyDebounce = debounce(applyFilter, 1000);
 
   const searchParams = new URLSearchParams(location.search);
   const query: string = searchParams.get('query')?.toLocaleLowerCase() || '';
+  const [value, setValue] = useState<string>();
 
   const filteredPeople = data.filter((person) => person.name.toLowerCase().includes(query)
     || person.fatherName?.toLowerCase().includes(query)
@@ -42,11 +53,12 @@ export const PeoplePage = () => {
           className="input"
           type="text"
           placeholder="enter person name"
-          value={query}
+          value={value}
           onChange={(event) => {
-            history.push({
-              search: `?query=${event.target.value}`,
-            });
+            if (event.target) {
+              setValue(event.target.value);
+              historyDebounce(event.target.value);
+            }
           }}
         />
       </div>
