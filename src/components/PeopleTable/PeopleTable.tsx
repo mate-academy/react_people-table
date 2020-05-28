b267cd8cd0ca1fc0ceaf7a3df8c348deef97e38a
Andreas_Just <andreas.wq@gmail.com>
2020-05-28 10:02:02 +0300

    fix historyPush

--- src/components/PeopleTable/PeopleTable.tsx
+++ src/components/PeopleTable/PeopleTable.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import cn from 'classnames';
-import { Table } from 'semantic-ui-react';
+import { Icon, Table } from 'semantic-ui-react';
 import TableCells from '../TableCells';
 import Paginator from '../Paginator';
 import './PeopleTable.scss';
@@ -8,37 +8,54 @@ import './PeopleTable.scss';
 type Props = {
   path: string;
   page: number;
+  perPage: number;
   totalPages: number;
+  sortedBy: string;
+  isSortedAsc: boolean;
   people: Person[];
   tableHeaders: TableHeader[];
   onSortTable: (field: string) => void;
   onSelectPerson: (field: string, person: Person) => void;
   onSelectPage: (_: React.SyntheticEvent, data: object) => void;
+  onSelectPerPage: (_: React.SyntheticEvent, data: object) => void;
 };
 
 const PeopleTable: React.FC<Props> = React.memo(
   ({
     path,
     page,
+    perPage,
     totalPages,
     people,
+    sortedBy,
+    isSortedAsc,
     tableHeaders,
     onSortTable,
     onSelectPerson,
     onSelectPage,
+    onSelectPerPage,
   }) => {
-    console.log('PeopleTable');
     return (
       <Table celled className="PeopleTable">
         <Table.Header>
-          <Table.Row>
+          <Table.Row className="PeopleTable-TableRow">
             {tableHeaders.map(({ name, code }) => (
               <Table.HeaderCell
                 key={code}
-                content={name}
-                className="sort-button"
+                className="PeopleTable-HeaderCell Th"
                 onClick={() => onSortTable(code)}
-              />
+              >
+                {sortedBy === code && (
+                  <Icon
+                    name={
+                      isSortedAsc ? 'arrow circle down' : 'arrow circle up'
+                    }
+                    className="PeopleTable-Icon"
+                    color="teal"
+                  />
+                )}
+                {name}
+              </Table.HeaderCell>
             ))}
           </Table.Row>
         </Table.Header>
@@ -48,7 +65,8 @@ const PeopleTable: React.FC<Props> = React.memo(
             <Table.Row
               key={person.id}
               warning={person.slug === path}
-              className={cn('PeopleTable-TableRow', {
+              className={cn({
+                'PeopleTable-TableRow': true,
                 'PeopleTable-TableRow_male': person.sex === 'm',
                 'PeopleTable-TableRow_female': person.sex === 'f',
               })}
@@ -63,11 +81,13 @@ const PeopleTable: React.FC<Props> = React.memo(
         </Table.Body>
 
         <Table.Footer>
-          <Table.Row>
+          <Table.Row className="PeopleTable-TableRow">
             <Paginator
               page={page}
+              perPage={perPage}
               totalPages={totalPages}
               onSelectPage={onSelectPage}
+              onSelectPerPage={onSelectPerPage}
             />
           </Table.Row>
         </Table.Footer>
