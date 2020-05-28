a654aeb97210569b537f8fbfcc5210a16a146ea7
kate <kate@test.com>
2020-05-28 16:06:40 +0300

    filtering and sorting added without debounce

--- src/PeoplePage.tsx
+++ src/PeoplePage.tsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import { useHistory, useLocation } from 'react-router-dom';
 import { PeopleTable } from './PeopleTable';
 import { getPeople } from './api';
 
@@ -11,7 +12,7 @@ function preparePerson(person: Person, all: Person[]): Person {
 }
 
 export const PeoplePage = () => {
-  const [data, setData] = useState<Person[]>();
+  const [data, setData] = useState<Person[]>([]);
 
   useEffect(() => {
     getPeople()
@@ -22,10 +23,34 @@ export const PeoplePage = () => {
       });
   });
 
+  const history = useHistory();
+  const location = useLocation();
+
+  const searchParams = new URLSearchParams(location.search);
+  const query: string = searchParams.get('query')?.toLocaleLowerCase() || '';
+
+  const filteredPeople = data.filter((person) => person.name.toLowerCase().includes(query)
+    || person.fatherName?.toLowerCase().includes(query)
+    || person.motherName?.toLowerCase().includes(query));
+
+
   return (
     <>
       <h3 className="title is-2 is-spaced header">People page</h3>
-      {data && (<PeopleTable people={data} />)}
+      <div className="control">
+        <input
+          className="input"
+          type="text"
+          placeholder="enter person name"
+          value={query}
+          onChange={(event) => {
+            history.push({
+              search: `?query=${event.target.value}`,
+            });
+          }}
+        />
+      </div>
+      {data && (<PeopleTable people={filteredPeople} />)}
     </>
   );
 };
