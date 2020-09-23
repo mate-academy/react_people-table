2ff15adfb958975022e639e90464c4ec3af5cfe1
Vasyl Tykhan <tykhan@MacBook-Pro-Vasyl.local>
2020-09-23 23:15:28 +0300

    people table

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,24 @@
 import React from 'react';
+import { Switch, Route, Link } from 'react-router-dom';
 
 import './App.css';
+import { People } from './components/People/People';
+import { LostPage } from './components/LostPage/LostPage';
 
 const App = () => (
   <div className="App">
-    <h1>People table</h1>
+    <nav>
+      <ul>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/people">People</Link></li>
+      </ul>
+    </nav>
+
+    <Switch>
+      <Route exact path="/"><h1>Home page</h1></Route>
+      <Route path="/people"><People /></Route>
+      <Route path="*"><LostPage /></Route>
+    </Switch>
   </div>
 );
 
