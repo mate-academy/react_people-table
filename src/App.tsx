111edc801708d4771fb81d3eaef2b6e01bc53a1c
mhrynko <glukym@gmail.com>
2022-08-11 12:57:58 +0300

    initial code

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,39 @@
 import React from 'react';
 
+import '@fortawesome/fontawesome-free/css/all.css';
+import 'bulma/css/bulma.css';
 import './App.scss';
 
-const App = () => (
-  <div className="App">
-    <h1>People table</h1>
-  </div>
-);
+import peopleFromServer from './people.json';
 
-export default App;
+export class App extends React.Component {
+  state = {};
+
+  render() {
+    return (
+      <div className="box">
+        <h1 className="title">People table</h1>
+
+        <table className="table is-striped is-narrow">
+          <thead>
+            <tr>
+              <th>name</th>
+              <th>sex</th>
+              <th>born</th>
+            </tr>
+          </thead>
+
+          <tbody>
+            {peopleFromServer.map(person => (
+              <tr key={person.slug}>
+                <td>{person.name}</td>
+                <td>{person.sex}</td>
+                <td>{person.born}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    );
+  }
+}
---
+++ src/types/Person.ts
@@ -0,0 +1,9 @@
+export interface Person {
+  born: number;
+  died: number;
+  fatherName: string | null;
+  motherName: string | null;
+  name: string;
+  sex: string;
+  slug: string;
+}
