ce404acffb3d33e4ccc0a01c01de960151439156
mhrynko <glukym@gmail.com>
2022-08-12 19:45:52 +0300

    after hooks

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import '@fortawesome/fontawesome-free/css/all.css';
 import 'bulma/css/bulma.css';
@@ -6,12 +6,35 @@ import './App.scss';
 
 import { Loader } from './components/Loader';
 import { PeopleTable } from './components/PeopleTable';
+import { PeopleTableHooks } from './components/PeopleTableHooks';
 
 type State = {
   loaded: boolean;
 };
 
-export class App extends React.Component<{}, State> {
+export const App = () => {
+  const [loaded, setLoaded] = useState(false);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLoaded(true);
+    }, 500);
+  }, []);
+
+  return (
+    <div className="box">
+      <h1 className="title">People table</h1>
+
+      {loaded ? (
+        <PeopleTableHooks />
+      ) : (
+        <Loader />
+      )}
+    </div>
+  );
+};
+
+export class App2 extends React.Component<{}, State> {
   state: Readonly<State> = {
     loaded: false,
   };
@@ -24,6 +47,20 @@ export class App extends React.Component<{}, State> {
 
   render() {
     const { loaded } = this.state;
+
+    /*
+    const useState2 = (initialValue: boolean) => {
+      const { loaded = initialValue } = this.state;
+
+      const setValue = (value: boolean) => {
+        this.setState({ loaded: value });
+      };
+
+      return [loaded, setValue];
+    };
+
+    const [isLoaded, setLoaded] = useState2(false);
+    */
 
     return (
       <div className="box">
