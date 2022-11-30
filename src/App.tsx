b01b9d7d127108b1acf167c586c8b732449540dc
Sasha Yatsykovych <olexandr.y@mate.academy>
2022-11-30 08:40:59 +0200

    versino 2

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,28 @@
-import React from 'react';
+import { FC, useEffect, useState } from 'react';
 
 import '@fortawesome/fontawesome-free/css/all.css';
 import 'bulma/css/bulma.css';
 import './App.scss';
 
-import peopleFromServer from './people.json';
-import { Person } from './types/Person';
 import { PeopleTable } from './components/PeopleTable/PeopleTable';
 import { Loader } from './components/Loader';
 
-interface State {
-  people: Person[],
-}
+export const App: FC = () => {
+  const [isLoaded, setIsLoaded] = useState(false);
 
-export class App extends React.Component<{}, State> {
-  state: Readonly<State> = {
-    people: [],
-  };
-
-  componentDidMount() {
+  useEffect(() => {
     setTimeout(() => {
-      this.setState({ people: peopleFromServer });
+      setIsLoaded(true);
     }, 1000);
-  }
+  }, []);
 
-  render() {
-    const { people } = this.state;
+  return (
+    <div className="box">
+      <h1 className="title">People table</h1>
 
-    return (
-      <div className="box">
-        <h1 className="title">People table</h1>
+      {!isLoaded && <Loader />}
 
-        {people.length > 0
-          ? <PeopleTable people={people} />
-          : <Loader />}
-      </div>
-    );
-  }
-}
+      {isLoaded && <PeopleTable />}
+    </div>
+  );
+};
--- src/components/PeopleTable/PeopleTable.tsx
+++ src/components/PeopleTable/PeopleTable.tsx
@@ -1,81 +1,159 @@
-import { Component } from 'react';
+import { useState } from 'react';
 import classNames from 'classnames';
 
+import peopleFromServer from '../../people.json';
 import { Person } from '../../types/Person';
 import './PeopleTable.scss';
 
-interface Props {
-  people: Person[]
-}
+export const PeopleTable = () => {
+  const [people, setPeople] = useState<Person[]>(peopleFromServer);
+  const [selectedPeople, setSelectedPeople] = useState<Person[]>([]);
 
-interface State {
-  selectedPerson: Person | null;
-}
-
-export class PeopleTable extends Component<Props, State> {
-  state: Readonly<State> = {
-    selectedPerson: null,
+  const handlePersonSelection = (personToAdd: Person) => {
+    setSelectedPeople((prevSelectedPeople) => ([
+      ...prevSelectedPeople,
+      personToAdd,
+    ]));
   };
 
-  handlePersonSelection = (person: Person | null) => {
-    this.setState({ selectedPerson: person });
-  }
+  const handlePersonUnselection = (personToRemove: Person) => {
+    setSelectedPeople((prevSelectedPeople) => prevSelectedPeople
+      .filter(
+        person => person.slug !== personToRemove.slug,
+      ));
+  };
 
-  isSelected = (person: Person) => {
-    return person.slug === this.state.selectedPerson?.slug;
-  }
+  const handleMoveDown = (personToMoveDown: Person) => {
+    const peopleCopy = [...people];
 
-  render() {
-    const { isSelected } = this;
-    const { people } = this.props;
-    const { selectedPerson } = this.state;
-
-    return (
-      <table className="table is-striped is-narrow">
-        <caption>{selectedPerson?.name || 'Nobody selected'}</caption>
-        <thead>
-          <tr>
-            <th>-</th>
-            <th>name</th>
-            <th>sex</th>
-            <th>born</th>
-          </tr>
-        </thead>
-
-        <tbody>
-          {people.map(person => (
-            <tr
-              key={person.slug}
-              className={classNames('Person', {
-                isSelected: isSelected(person),
-              })}
-            >
-              <td>
-                {isSelected(person)
-                  ? (
-                    <button
-                      type="button"
-                      onClick={() => this.handlePersonSelection(null)}
-                    >
-                      -
-                    </button>
-                  )
-                  : (
-                    <button
-                      type="button"
-                      onClick={() => this.handlePersonSelection(person)}
-                    >
-                      +
-                    </button>
-                  )}
-              </td>
-              <td>{person.name}</td>
-              <td>{person.sex}</td>
-              <td>{person.born}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
+    const personIndex = peopleCopy.findIndex(
+      person => person.slug === personToMoveDown.slug,
     );
-  }
+
+    if (personIndex === peopleCopy.length - 1) {
+      return;
+    }
+
+    setPeople((prevPeople) => ([
+      ...prevPeople.slice(0, personIndex),
+      prevPeople[personIndex + 1],
+      prevPeople[personIndex],
+      ...prevPeople.slice(personIndex + 2),
+    ]));
+  };
+
+  const handleMoveUp = (personToMoveDown: Person) => {
+    const peopleCopy = [...people];
+
+    const personIndex = peopleCopy.findIndex(
+      person => person.slug === personToMoveDown.slug,
+    );
+
+    if (personIndex === 0) {
+      return;
+    }
+
+    const swap = peopleCopy[personIndex];
+
+    peopleCopy[personIndex] = peopleCopy[personIndex - 1];
+    peopleCopy[personIndex - 1] = swap;
+
+    setPeople(peopleCopy);
+  };
+
+  const isSelected = (personToCheck: Person) => {
+    return selectedPeople.some(
+      person => person.slug === personToCheck.slug,
+    );
+  };
+
+  const clearSelectedPeople = () => {
+    setSelectedPeople([]);
+  };
+
+  return (
+    <table className="table is-striped is-narrow">
+      <caption
+        className="title is-5 has-text-info"
+      >
+        <button
+          type="button"
+          className="delete"
+          onClick={clearSelectedPeople}
+        >
+          Clear all
+        </button>
+        {selectedPeople.map(person => person.name).join(', ')}
+      </caption>
+      <thead>
+        <tr>
+          <th>-</th>
+          <th>name</th>
+          <th>sex</th>
+          <th>born</th>
+          <th>reorder</th>
+        </tr>
+      </thead>
+
+      <tbody>
+        {people.map(person => (
+          <tr
+            key={person.slug}
+            className={classNames('Person', {
+              'has-background-warning': isSelected(person),
+            })}
+          >
+            <td>
+              {isSelected(person)
+                ? (
+                  <button
+                    type="button"
+                    onClick={() => handlePersonUnselection(person)}
+                  >
+                    <span className="icon is-small">
+                      <i className="fas fa-minus" />
+                    </span>
+                  </button>
+                )
+                : (
+                  <button
+                    type="button"
+                    onClick={() => handlePersonSelection(person)}
+                  >
+                    <span className="icon is-small">
+                      <i className="fas fa-plus" />
+                    </span>
+                  </button>
+                )}
+            </td>
+            <td className={classNames({
+              'has-text-link': person.sex === 'm',
+              'has-text-danger': person.sex === 'f',
+            })}
+            >
+              {person.name}
+            </td>
+
+            <td>{person.sex}</td>
+            <td>{person.born}</td>
+            <td>
+              <button
+                type="button"
+                onClick={() => handleMoveDown(person)}
+              >
+                &darr;
+              </button>
+
+              <button
+                type="button"
+                onClick={() => handleMoveUp(person)}
+              >
+                &uarr;
+              </button>
+            </td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
 }
