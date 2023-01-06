e55b31296e43a8eec437d6601e6a44ae382483cd
Misha Hrynko <misha@mate.academy>
2023-01-06 10:40:44 +0200

    add loading and empty state

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { Person } from './types/Person';
+import { Loader } from './components/Loader';
 
 import '@fortawesome/fontawesome-free/css/all.css';
 import 'bulma/css/bulma.css';
@@ -6,33 +8,62 @@ import './App.scss';
 
 import peopleFromServer from './people.json';
 
-export class App extends React.Component {
-  state = {};
+type State = {
+  isDataLoaded: boolean;
+  people: Person[];
+};
+
+export class App extends React.Component<{}, State> {
+  state: State = {
+    isDataLoaded: false,
+    people: [],
+  };
+
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({
+        isDataLoaded: true,
+        people: peopleFromServer,
+      });
+    }, 500);
+  }
 
   render() {
+    const { people, isDataLoaded } = this.state;
+
     return (
       <div className="box">
         <h1 className="title">People table</h1>
 
-        <table className="table is-striped is-narrow">
-          <thead>
-            <tr>
-              <th>name</th>
-              <th>sex</th>
-              <th>born</th>
-            </tr>
-          </thead>
+        {!isDataLoaded && (
+          <Loader />
+        )}
 
-          <tbody>
-            {peopleFromServer.map(person => (
-              <tr key={person.slug}>
-                <td>{person.name}</td>
-                <td>{person.sex}</td>
-                <td>{person.born}</td>
+        {isDataLoaded && people.length === 0 && (
+          <p>No people yet</p>
+        )}
+
+        {isDataLoaded && people.length > 0 && (
+          <table className="table is-striped is-narrow">
+            <thead>
+              <tr>
+                <th>name</th>
+                <th>sex</th>
+                <th>born</th>
               </tr>
-            ))}
-          </tbody>
-        </table>
+            </thead>
+
+            <tbody>
+              {people.map(person => (
+                <tr key={person.slug}>
+                  <td>{person.name}</td>
+                  <td>{person.sex}</td>
+                  <td>{person.born}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        )}
       </div>
     );
   }
