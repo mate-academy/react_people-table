4d8176ab7a112e0634e88355a48f7b889ff9f77f
Misha Hrynko <misha@mate.academy>
2023-07-05 11:55:35 +0300

    filter people

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,31 @@ import classNames from 'classnames';
 import peopleFromServer from './people.json';
 import { Button } from './components/Button/Button';
 
+function getFilteredPeople(people, query, sex) {
+  let filteredPeople = people;
+
+  if (query) {
+    const normalizedQuery = query.toLowerCase();
+
+    filteredPeople = filteredPeople.filter(
+      person => person.name.toLowerCase().startsWith(normalizedQuery),
+    );
+  }
+
+  if (sex !== 'all') {
+    filteredPeople = filteredPeople.filter(person => person.sex === sex);
+  }
+
+  return filteredPeople;
+}
+
 export const App = ({ user }) => {
+  const [query, setQuery] = useState('');
+  const [sex, setSex] = useState('all');
+
+  const visiblePeople = getFilteredPeople(peopleFromServer, query, sex);
+
+  // #region selected
   const [selectedPeople, setSelectedPeople] = useState([]);
 
   const isSelected = personToCheck => selectedPeople.some(
@@ -19,9 +43,21 @@ export const App = ({ user }) => {
       selectedPeople.filter(person => person.slug !== personToRemove.slug),
     );
   };
+  // #endregion
 
   return (
     <div className="box">
+      <button type="button" onClick={() => setSex('all')}>all</button>
+      <button type="button" onClick={() => setSex('m')}>m</button>
+      <button type="button" onClick={() => setSex('f')}>f</button>
+      {sex}
+
+      <input
+        type="search"
+        value={query}
+        onChange={event => setQuery(event.target.value)}
+      />
+
       <table className="table is-striped is-narrow">
         <caption className="title is-5 has-text-info">
           {selectedPeople
@@ -40,7 +76,7 @@ export const App = ({ user }) => {
         </thead>
 
         <tbody>
-          {peopleFromServer.map(person => (
+          {visiblePeople.map(person => (
             <tr
               key={person.slug}
               className={classNames({
