87fc0742241402aa9ca6bf9cc1ce0ff14ce73c58
mykhalenych <mykhalenych@gmail.com>
2024-02-15 13:31:41 +0200

    feature/prettier added prettier, updated packages

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,4 @@
 import { createRoot } from 'react-dom/client';
 import { App } from './App';
 
-const root = createRoot(
-  document.getElementById('root') as HTMLDivElement,
-);
-
-root.render(<App />);
+createRoot(document.getElementById('root') as HTMLElement).render(<App />);
